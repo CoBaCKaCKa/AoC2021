df11eb61e1a5b7d5caded8c702db9ff700aec503
CoBaCKaCKa <95453201+CoBaCKaCKa@users.noreply.github.com>
2021-12-04 01:53:40 +0000

    Create Day3.jl

---
+++ Day3.jl
@@ -0,0 +1,27 @@
+path = "C:\\AoC2021\\Day3.txt"
+data = readlines(path)
+
+# Part 1
+h = size(data,1); w = length(data[1]); u = zeros(w)
+for iC=1:w
+  thisCol = map((x)->parse(Int,x[iC]),data)
+  u[iC] = sum(thisCol)>h/2
+end
+v = 1 .- u
+prod(sum([u v] .* (2 .^ ((w-1):-1:0)),dims=1))
+
+# Part 2
+dataLeft = data; iC = 1
+while size(dataLeft,1)>1
+  thisCol = map((x)->parse(Int,x[iC]),dataLeft)
+  if sum(thisCol)>=size(dataLeft,1)/2
+    mostCommon = 1
+  else
+    mostCommon = 0
+  end
+  dataLeft = dataLeft[thisCol.==mostCommon]
+  iC += 1
+end
+
+# Run the above once with ==mostCommon and once with != and keep the output
+parse(Int,dataLeft1[1],base=2).*parse(Int,dataLeft2[1],base=2)
