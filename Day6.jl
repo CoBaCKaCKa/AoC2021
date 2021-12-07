e9a53f56cb346f52695f20eafcc2d6a2ecaa3094
CoBaCKaCKa <95453201+CoBaCKaCKa@users.noreply.github.com>
2021-12-07 01:33:27 +0000

    Update Day6.jl

--- Day6.jl
+++ Day6.jl
@@ -6,12 +6,7 @@ cts = [count(==(i), data) for i in collect(0:8)]
 function nbFish(cts,nbDays)
   for i=1:nbDays
     prevZeros = cts[1]
-    cts[1] = cts[2]
-    cts[2] = cts[3]
-    cts[3] = cts[4]
-    cts[4] = cts[5]
-    cts[5] = cts[6]
-    cts[6] = cts[7]
+    [cts[j] = cts[j+1] for j=1:6]
     cts[7] = cts[8] + prevZeros
     cts[8] = cts[9]
     cts[9] = prevZeros
